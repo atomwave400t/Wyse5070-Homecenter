daebef3ca5725783849610296a2dd70d801bbb87
atomwave400t <krzysztoff.kunysz@gmail.com>
2026-08-26 23:46:41 +0200

    add 445 port rule

--- scripts/k3s_setup.sh
+++ scripts/k3s_setup.sh
@@ -21,6 +21,7 @@ echo ""
 firewall-cmd --permanent --add-port=6443/tcp #apiserver
 firewall-cmd --permanent --zone=trusted --add-source=10.42.0.0/16 #pods
 firewall-cmd --permanent --zone=trusted --add-source=10.43.0.0/16 #services
+firewall-cmd --zone=public --add-port=445/tcp --permanent
 firewall-cmd --reload
 
 export PATH="$PATH:/usr/local/bin"
